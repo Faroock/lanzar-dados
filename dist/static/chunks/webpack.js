74722a5a41a0f857401872c5446d0b1c50db255e
Rodrigo Iturrieta <riturrieta@brainfood.cl>
2022-08-13 15:28:02 -0400

    Primer commit

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e08f279cf0846605"; }
+/******/ 		__webpack_require__.h = function() { return "1c77ced041ab5d3f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/services/sockets.js
+++ src/services/sockets.js
@@ -2,6 +2,7 @@ import io from 'socket.io-client';
 
 const socket = io('https://lanzar-dados-back.herokuapp.com/', {
     transports: ['websocket'],
+    secure: true,
 });
 
 export default socket;
